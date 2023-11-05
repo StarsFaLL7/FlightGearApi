809605220e7a5601118c6cd6e0441bdaf2c82acb
StarsFaLL7 <starsfall-ru@yandex.ru>
2023-11-05 15:33:16 +0500

    Deleted old files

--- FlightGearApi/FlightGearStart.bat
+++
@@ -1,3 +0,0 @@
-SET FG_ROOT=E:\Programs\FlightGear 2020.3\data
-cd E:\Programs\FlightGear 2020.3\bin
-fgfs --aircraft=c172p --disable-clouds --disable-sound --in-air --enable-freeze --airport=KSFO --altitude=7224 --heading=113
