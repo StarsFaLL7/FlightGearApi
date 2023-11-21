791a12772f8ca11f10fd4618d93e11ce197c0b93
StarsFaLL7 <starsfall-ru@yandex.ru>
2023-11-21 20:52:37 +0500

    Added more properties to PropertiesHelper

--- FlightGearApi/Domain/UtilityClasses/FlightPropertiesHelper.cs
+++ FlightGearApi/Domain/UtilityClasses/FlightPropertiesHelper.cs
@@ -2,29 +2,93 @@
 
 namespace FlightGearApi.Domain.UtilityClasses;
 
-public enum FlightProperties
+public enum UtilityProperty
 {
-    Throttle,
-    AltitudeMeters
+    Throttle, // Мощность двигателя
+    Aileron, // Наклон штурвала
+    Elevator, // Положение штурвала (на/от себя)
+    Rudder, // Прокрутка самолёта для управления рысканием
+    SpeedBrake, // Воздушные тормозы
+    Altitude, // Высота в метрах
+    Roll, // Крен
+    Pitch, // Тангаж
+    Heading // Рыскание
 }
 
-public static class FlightPropertiesInfo
+public static class FlightPropertiesHelper
 {
-    public static Dictionary<FlightProperties, FlightPropertyInfo> Properties { get; } = new ()
+    public static Dictionary<UtilityProperty, FlightPropertyInfo> OutputProperties { get; } = new ()
     {
-        { FlightProperties.Throttle, new FlightPropertyInfo(
-                "/controls/engines/current-engine/throttle",
-                "Throttle In", 
-                typeof(double),
-                "double",
-                "%.5f")
-        },
-        { FlightProperties.AltitudeMeters, new FlightPropertyInfo(
+        { UtilityProperty.Altitude, new FlightPropertyInfo(
             "/position/altitude-agl-m", 
-            "Altitude in Meters", 
+            "altitude-agl-m", 
             typeof(double),
             "double",
-            "%.5f")
-        }
+            "%.5f") },
+        { UtilityProperty.Roll, new FlightPropertyInfo(
+            "/orientation/roll-deg", 
+            "roll-deg", 
+            typeof(double),
+            "double",
+            "%.5f") },
+        { UtilityProperty.Pitch, new FlightPropertyInfo(
+            "/orientation/pitch-deg", 
+            "pitch-deg", 
+            typeof(double),
+            "double",
+            "%.5f") },
+        { UtilityProperty.Heading, new FlightPropertyInfo(
+            "/orientation/heading-deg", 
+            "heading-deg", 
+            typeof(double),
+            "double",
+            "%.5f") },
     };
+
+    public static Dictionary<UtilityProperty, (FlightPropertyInfo Property, double MinValue, double MaxValue)> InputProperties { get; } = new()
+    {
+        { UtilityProperty.Throttle, (new FlightPropertyInfo(
+            "/controls/engines/current-engine/throttle",
+            "throttle", 
+            typeof(double),
+            "double",
+            "%.5f"), 
+            0, 1)},
+        { UtilityProperty.Aileron, (new FlightPropertyInfo(
+            "/controls/flight/aileron",
+            "aileron", 
+            typeof(double),
+            "double",
+            "%.5f"), 
+            -1, 1)},
+        { UtilityProperty.Elevator, (new FlightPropertyInfo(
+            "/controls/flight/elevator",
+            "elevator", 
+            typeof(double),
+            "double",
+            "%.5f"), 
+            -1, 1)},
+        { UtilityProperty.Rudder, (new FlightPropertyInfo(
+            "/controls/flight/rudder",
+            "rudder", 
+            typeof(double),
+            "double",
+            "%.5f"), 
+            -1, 1)},
+        { UtilityProperty.SpeedBrake, (new FlightPropertyInfo(
+                "/controls/flight/speedbrake",
+                "speedbrake", 
+                typeof(double),
+                "double",
+                "%.5f"), 
+            0, 1)}
+    };
+
+    public static List<FlightPropertyInfo> AllUtilityProperties()
+    {
+        return InputProperties
+            .Select(p => p.Value.Property)
+            .Concat(OutputProperties.Values)
+            .ToList();
+    }
 }
