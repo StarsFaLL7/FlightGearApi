6cd2038ebe4678c15fc39d2cf21c6c9e8045ff1c
xMAPKEPx <russia201554321@gmail.com>
2023-12-29 23:06:52 +0500

    Проверка, если что-то сломается - откатить

--- FlightGearApi/ClientApp/src/redux/ChartSlice/ChartSlice.js
+++ FlightGearApi/ClientApp/src/redux/ChartSlice/ChartSlice.js
@@ -5,7 +5,7 @@ const chartSlice = createSlice({
   initialState: {
     data: [],
     isReloading: 0,
-    currentSession: 1
+    currentSession: null
   },
   reducers: {
     setData(state, action) {
