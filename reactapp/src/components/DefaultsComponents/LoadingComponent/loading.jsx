c7b22fee527372018f87a6e5bec368c678f05b00
Дмитрий <dimadrob2004@mail.ru>
2024-06-13 22:03:16 +0500

    deleted-useless

--- reactapp/src/components/DefaultsComponents/LoadingComponent/loading.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactLoading from "react-loading";
- 
-export default function Loading() {
-    return (
-    <ReactLoading type="spin" color="#0000FF"
-        height={50} width={50} />
-    );
-}
